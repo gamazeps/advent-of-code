b081fed97663218ea81a20f6b67ab38fc6d7ca92
Jon Harper <jon.harper87@gmail.com>
2017-12-10 20:01:09 +0100

    Add a unit test sigh

--- events.cc
+++ events.cc
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <vector>
 #include <set>
+#include <iostream>
 
 using namespace std;
 
@@ -8,7 +9,21 @@ struct event_t {
     int begin;
     int end;
     int id; // lowest the better
+
+    friend std::ostream & operator<<(std::ostream &os, const struct event_t& p);
+    bool operator ==(const struct event_t &b) const {
+        return this->begin == b.begin && this->end == b.end && this->id == b.id;
+    }
 };
+
+    std::ostream& operator<<(
+            std::ostream& os, // stream object
+            const struct event_t& foo
+        )
+        {
+            os << "{" << foo.begin << ","<< foo.end << ","<< foo.id << "}";
+            return os;
+        }
 
 bool starts_before(event_t a, event_t b) {
     return a.begin < b.begin;
@@ -67,6 +82,32 @@ vector<event_t> schedule(vector<event_t> input) {
     return res;
 }
 
+void dumpv(vector<struct event_t> &vec) {
+    for (int i=0; i<vec.size();i++) {
+        cout << vec[i];
+        if (i < (vec.size()-1)) {
+            cout << ",";
+        }
+    }
+}
+
 int main() {
+    bool ok = true;
+
+    {
+    struct event_t e1 = { 10, 20, 1 };
+    struct event_t arr[] = { e1 };
+    vector<struct event_t> v(arr, arr+ sizeof(arr)/sizeof(event_t));
+    vector<struct event_t> res = schedule(v);
+
+    struct event_t e1_ref = { 10, 20, 1 };
+    struct event_t arr_ref[] = { e1_ref };
+    vector<struct event_t> v_ref(arr_ref, arr_ref + sizeof(arr_ref)/sizeof(event_t));
+
+    bool eq = v == v_ref;
+    ok &= eq;
+    std::cout << (eq ? "OK" : "KO") << " : expected " ; dumpv(v); std::cout << ", got " ; dumpv(v_ref) ; std::cout << std::endl;
+    }
+
     return 0;
 }
