e5deb75cf0350bb09a29b29b7c48373852bb2078
Felix Raimundo <gamaz3ps@gmail.com>
2017-12-10 18:52:35 +0100

    make compile

--- events.cc
+++ events.cc
@@ -1,15 +1,29 @@
-typedef struct event_t {
+#include <algorithm>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+struct event_t {
     int begin;
     int end;
     int id; // lowest the better
-} event_t;
+};
+
+bool starts_before(event_t a, event_t b) {
+    return a.begin < b.begin;
+}
+
+bool ends_before(event_t a, event_t b) {
+    return a.end < b.end;
+}
 
 vector<event_t> schedule(vector<event_t> input) {
     // First generate the list of starts and ends
     vector<event_t> begs(input);
-    sort(begs.begin(), begs.end(), begin_time); // TODO: impl sort by begin;
+    sort(begs.begin(), begs.end(), starts_before);
     vector<event_t> ends(input);
-    sort(begs.begin(), begs.end(), end_time); // TODO: impl sort by begin;
+    sort(begs.begin(), begs.end(), ends_before);
 
     vector<event_t> res;
 
@@ -22,7 +36,7 @@ vector<event_t> schedule(vector<event_t> input) {
         event_t end = ends[e];
         if(beg.begin < end.end) {
             currents.insert(beg.id);
-            if (currents.size == 1) {
+            if (currents.size() == 1) {
                 curr_e = beg; // this is now our only event.
             }
             else if (beg.id < curr_e.id) {
@@ -38,7 +52,7 @@ vector<event_t> schedule(vector<event_t> input) {
                 res.push_back(curr_e);
                 // Note, could be implemented with a heap, but too lazy.
                 if (currents.size() > 0) {
-                    int new_id = max_elem(currents.begin(), currents.end());
+                    set<int>::iterator new_id = max_element(currents.begin(), currents.end());
                     curr_e.id = *new_id;
                     curr_e.begin = end.end;
                 }
@@ -48,4 +62,8 @@ vector<event_t> schedule(vector<event_t> input) {
     }
 
     return res;
+}
+
+int main() {
+    return 0;
 }
